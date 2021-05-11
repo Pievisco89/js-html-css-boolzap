2e451022a68b4102107aebdc8a47d9f09a5629c3
Pievisco89 <pie-89@live.it>
2021-05-11 18:16:37 +0200

    da finire Milestone 5

--- Milestone 5/assets/js/script.js
+++ Milestone 5/assets/js/script.js
@@ -18,17 +18,17 @@ const app = new Vue({
           {
             date: '10/01/2020 15:30:55',
             text: 'Hai portato a spasso il cane?',
-            status: 'sent'
+            status: 'sent',
           },
           {
             date: '10/01/2020 15:50:00',
             text: 'Ricordati di dargli da mangiare',
-            status: 'sent'
+            status: 'sent',
           },
           {
             date: '10/01/2020 16:15:22',
             text: 'Tutto fatto!',
-            status: 'received'
+            status: 'received',
           }
         ],
       },
@@ -40,17 +40,17 @@ const app = new Vue({
           {
             date: '20/03/2020 16:30:00',
             text: 'Ciao come stai?',
-            status: 'sent'
+            status: 'sent',
           },
           {
             date: '20/03/2020 16:30:55',
             text: 'Bene grazie! Stasera ci vediamo?',
-            status: 'received'
+            status: 'received',
           },
           {
             date: '20/03/2020 16:35:00',
             text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-            status: 'sent'
+            status: 'sent',
           }
         ],
       },
@@ -62,17 +62,17 @@ const app = new Vue({
           {
             date: '28/03/2020 10:10:40',
             text: 'La Marianna va in campagna',
-            status: 'received'
+            status: 'received',
           },
           {
             date: '28/03/2020 10:20:10',
             text: 'Sicuro di non aver sbagliato chat?',
-            status: 'sent'
+            status: 'sent',
           },
           {
             date: '28/03/2020 16:15:22',
             text: 'Ah scusa!',
-            status: 'received'
+            status: 'received',
           }
         ],
       },
@@ -84,18 +84,16 @@ const app = new Vue({
           {
             date: '10/01/2020 15:30:55',
             text: 'Lo sai che ha aperto una nuova pizzeria?',
-            status: 'sent'
+            status: 'sent',
           },
           {
             date: '10/01/2020 15:50:00',
             text: 'Si, ma preferirei andare al cinema',
-            status: 'received'
+            status: 'received',
           }
         ],
       },
     ],
-
-    timeMessage: '',
 
   },
   methods:{
@@ -136,16 +134,10 @@ const app = new Vue({
       });
     },
 
-    //funzione per prendere date dell'ultimo messaggio 'received'
-    getTimeMessage(){
-      this.contacts.forEach((contact) => {
-        if(contact.messages.status === 'received'){
-          return this.timeMessage = (contact.messages.length - 1).date  
-        }
-      });
-    },
-
-
+    //funzione per cancellare messaggio 
+    deleteMessage(index){
+      this.contacts[this.activeUser].messages.splice(index, 1);
+    }
 
   },
 });
